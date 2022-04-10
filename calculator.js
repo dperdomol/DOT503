f2487cc7f77ca52bd98f97ece6b4b752199a6357
DiegoPerdomo <diegoivanperdomoleon@gmail.com>
2022-04-10 18:02:23 +0930

    Multiplication function was added

--- calculator.js
+++ calculator.js
@@ -5,7 +5,13 @@ function addNumbers(num1, num2){
     return res;
 }
 
+function multiplication(num1, num2){
+    let res = num1 * num2;
+    return res;
+}
+
 const addButton = document.getElementById("addButton");
+const mulButton = document.getElementById("mulButton");
 
 addButton.addEventListener("click", function(){
 
@@ -17,4 +23,16 @@ addButton.addEventListener("click", function(){
     console.log(addNumbers(inp1, inp2));
 
     document.getElementById("result").innerHTML = addNumbers(inp1, inp2);
+});
+
+mulButton.addEventListener("click", function(){
+
+    let inp1 = document.getElementById("inp1").value;
+    let inp2 = document.getElementById("inp2").value;
+
+    inp1 = parseInt(inp1);
+    inp2 = parseInt(inp2);
+    console.log(multiplication(inp1, inp2));
+
+    document.getElementById("result").innerHTML = multiplication(inp1, inp2);
 });
